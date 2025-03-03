29883a7113f6ccf9f5d7996d96dc70eae31cf3bc
Yuliia Skrypnyk <jillyjoolli@gmail.com>
2025-03-03 16:10:26 +0100

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,47 @@
+let age: number = 30;
+
+for(let i = 1; i <= age; i++) {
+    console.log(i);
+}
+
+if(age > 18) {
+    console.log("You are an adult.");
+} else {
+    console.log("You are a minor.");
+}
+
+let score: number = 0;
+
+if(score !== 0) {
+    console.log("Score is available.");
+}
+
+if(score) {
+    console.log("Score is evaluated as truthy.");
+} else {
+    console.log("Score is evaluated as falsy.");
+}
+
+let username: string = "";
+
+if(username !== "") {
+    console.log("Username is available.");
+}
+
+if(username) {
+    console.log("Username is evaluated as truthy.");
+} else {
+    console.log("Username is evaluated as falsy.");
+}
+
+let isAdmin: boolean = false;
+
+if(isAdmin) {
+    console.log("isAdmin is evaluated as truthy.");
+} else {
+    console.log("isAdmin is evaluated as falsy.");
+}
+
+if(!isAdmin) {
+    console.log("isAdmin is false.");
+}
