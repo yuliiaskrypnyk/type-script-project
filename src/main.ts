b7453a0c85c6cbd4865d4bbc8ff8bd1fc260f677
Yuliia Skrypnyk <jillyjoolli@gmail.com>
2025-03-03 16:37:44 +0100

    Add ChristmasTree

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 let age: number = 30;
 
-for(let i = 1; i <= age; i++) {
+for (let i = 1; i <= age; i++) {
     console.log(i);
 }
 
-if(age > 18) {
+if (age > 18) {
     console.log("You are an adult.");
 } else {
     console.log("You are a minor.");
@@ -12,11 +12,11 @@ if(age > 18) {
 
 let score: number = 0;
 
-if(score !== 0) {
+if (score !== 0) {
     console.log("Score is available.");
 }
 
-if(score) {
+if (score) {
     console.log("Score is evaluated as truthy.");
 } else {
     console.log("Score is evaluated as falsy.");
@@ -24,11 +24,11 @@ if(score) {
 
 let username: string = "";
 
-if(username !== "") {
+if (username !== "") {
     console.log("Username is available.");
 }
 
-if(username) {
+if (username) {
     console.log("Username is evaluated as truthy.");
 } else {
     console.log("Username is evaluated as falsy.");
@@ -36,12 +36,26 @@ if(username) {
 
 let isAdmin: boolean = false;
 
-if(isAdmin) {
+if (isAdmin) {
     console.log("isAdmin is evaluated as truthy.");
 } else {
     console.log("isAdmin is evaluated as falsy.");
 }
 
-if(!isAdmin) {
+if (!isAdmin) {
     console.log("isAdmin is false.");
 }
+
+function printChristmasTree(n: number): void {
+    for (let i = 0; i < n; i++) {
+        let spaces = ' '.repeat(n - i);
+        let stars = '*'.repeat(2 * i + 1);
+        console.log(spaces + stars);
+    }
+
+    for (let i = 0; i < 5; i++) {
+        console.log(' '.repeat(n) + '*');
+    }
+}
+
+printChristmasTree(10);
