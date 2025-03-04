a49498258d6c9682d7cee738625bc04ebe859f34
Yuliia Skrypnyk <jillyjoolli@gmail.com>
2025-03-04 12:01:52 +0100

    Add Functions

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-let age: number = 30;
+let age: number = 10;
 
 for (let i = 1; i <= age; i++) {
     console.log(i);
@@ -46,16 +46,65 @@ if (!isAdmin) {
     console.log("isAdmin is false.");
 }
 
+console.log("----------------------");
 function printChristmasTree(n: number): void {
     for (let i = 0; i < n; i++) {
-        let spaces = ' '.repeat(n - i);
-        let stars = '*'.repeat(2 * i + 1);
+        let spaces = ' ' . repeat(n - i);
+        let stars = '*' . repeat(2 * i + 1);
         console.log(spaces + stars);
     }
 
-    for (let i = 0; i < 5; i++) {
-        console.log(' '.repeat(n) + '*');
+    for (let i = 0; i < n; i++) {
+        console.log(' ' . repeat(n) + '*');
     }
 }
 
-printChristmasTree(10);
+printChristmasTree(5);
+
+console.log("----------------------");
+const numbers = [1, 2, 3];
+const doubled = numbers.map(num => num * 2);
+console.log(doubled);
+
+const words = ["kiwi", "apple", "banana"];
+const longwWords = words.filter(word=> word.length > 5);
+console.log(longwWords);
+
+const nums = [12, 13, 14];
+const sum = nums.reduce((accumulator, number) => accumulator + number, 0);
+console.log(sum);
+
+const values = [5, 20, -1];
+const hasLargeNumber = values.some(num => num > 10);
+console.log(hasLargeNumber);
+
+console.log("----------------------");
+const list = [15, 6, 3213, 9, 0, 12, 8464 , 1, 1264, 481, 186, 1031, 194];
+
+const sortedList = list.sort((a, b): number => b - a);
+console.log(sortedList);
+
+const squaredList  = sortedList.map(num => num ** 2); // num * num
+console.log(squaredList);
+
+const trimmedList = squaredList.slice(4, -2);
+console.log(trimmedList);
+
+const filteredList = trimmedList.filter(num => num % 4 !== 0);
+console.log(filteredList);
+
+const sumList = filteredList.reduce((acc, num) => acc + num, 0);
+console.log(sumList);
+
+console.log("----------------------");
+const list1 = [15, 6, 3213, 9, 0, 12, 8464, 1, 1264, 481, 186, 1031, 194];
+
+const sumList1 = list1
+    .sort((a, b): number => b - a)
+    .map(num => num ** 2)
+    .slice(4, -2)
+    .filter(num => num % 4 !== 0)
+    .reduce((acc, num) => acc + num, 0);
+
+console.log(sumList1);
+
